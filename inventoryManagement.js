e91140c330967c1b15a62d64dbe83cddee1ecf82
Austin Vanloon <wic404learn@gmail.com>
2025-05-10 13:30:13 -0600

    Initial commit

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -3,10 +3,28 @@
 
 
 // Export the necessary parts for testing
+let products = ["Laptop", "Phone", "Headphones", "Monitor"];
+
+function logFirstProduct() {
+  console.log(products[0]);
+}
+
+function addProduct(productName) {
+  products.push(productName);
+}
+
+function updateProductName(index, newName) {
+  products[index] = newName;
+}
+
+function removeLastProduct() {
+  products.pop();
+}
+
 module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
+  products,
+  logFirstProduct,
+  addProduct,
+  updateProductName,
+  removeLastProduct
 };
